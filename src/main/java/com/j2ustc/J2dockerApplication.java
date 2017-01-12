ad6c6d1de18af4e3635a391f65f72cbf456c4a1f
Yenole <Netxy@vip.qq.com>
2017-01-12 17:09:50 +0800

    init project

---
+++ src/main/java/com/j2ustc/J2dockerApplication.java
@@ -0,0 +1,12 @@
+package com.j2ustc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2dockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2dockerApplication.class, args);
+	}
+}
